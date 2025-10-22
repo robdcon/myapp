d7565e24320e23b32275120e4f69f562213cb526
RConnolly <rconnolly@macmillan.org.uk>
2025-10-22 07:44:27 +0100

    add provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ApolloWrapper>
-          {children}
+          <Provider>
+            {children}
+          </Provider>
         </ApolloWrapper>
       </body>
     </html>
